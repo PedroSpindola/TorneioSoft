322ebe50b0e6da4bed88af6cb39a164cf5213242
PedroSpindola <pedrojf04@gmail.com>
2025-04-05 15:49:51 -0300

    criação do componente navbar

---
+++ torneio-soft/src/pages/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import 'bootswatch/dist/vapor/bootstrap.css'
+import Navbar from "../components/navbar";
+
+function Home() {
+
+    return (
+
+
+        <Navbar/>
+
+    );
+
+}
+
+export default Home;
